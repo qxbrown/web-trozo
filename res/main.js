2a5f39da4fc326da4f1c49be274a7bfe7631150e
Brown <90845793+heybrown@users.noreply.github.com>
2021-10-26 13:47:05 +0545

    Add files via upload

---
+++ res/main.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+  loop: false,
+  margin: 15,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
